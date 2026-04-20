ebcad9c46ddf9b578bac09f25dd9be83d7a8fad3
Tiziano Chairini <tiziano03chiarini@gmail.com>
2026-04-20 17:28:48 +0200

    Chore: configurato Gradle per il supporto a JavaFX e impostata GameGUI come Main Class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application") // Permette a Gradle di avviare la nostra app grafica
+    id("org.openjfx.javafxplugin") version "0.0.13" // Il plugin ufficiale di JavaFX
 }
 
 group = "it.unicam.cs.mpgc.rpg123024"
@@ -7,6 +9,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+// Configurazione di JavaFX
+javafx {
+    version = "21" // Usiamo la versione allineata al tuo Java 21
+    modules = listOf("javafx.controls", "javafx.graphics")
+}
+
+// Diciamo a Gradle quale sarà il punto di partenza della grafica (creeremo questa classe tra poco!)
+application {
+    mainClass.set("it.unicam.cs.mpgc.rpg123024.GameGUI")
 }
 
 dependencies {
